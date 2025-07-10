f95ec5427b0a1db45510a977fe1dc8d3f101ceb4
nd55 <45429767+nd55@users.noreply.github.com>
2025-07-10 11:27:19 +0200

    animate details element with CSS only, upgrade deps, fix meta tags bug

--- src/components/Calculator/Calculator.svelte
+++ src/components/Calculator/Calculator.svelte
@@ -324,14 +324,10 @@
   </div>
 </section>
 
-<details-utils animate>
-  <details class={styles.liveExample} style="--variable-value: {result}">
-    <summary>Live font-size example</summary>
-    <div>
-      <p style="font-size:var(--variable-value)" contenteditable="true">
-        This text uses the calculated clamp formula. It gets invisible when you
-        use 0 or negative numbers as a min-value. 🥲 Try editing it, though...
-      </p>
-    </div>
-  </details>
-</details-utils>
+<details class={styles.liveExample} style="--variable-value: {result}">
+  <summary>Live font-size example</summary>
+  <p style="font-size:var(--variable-value)" contenteditable="true">
+    This text uses the calculated clamp formula. It gets invisible when you use
+    0 or negative numbers as a min-value. 🥲 Try editing it, though...
+  </p>
+</details>
